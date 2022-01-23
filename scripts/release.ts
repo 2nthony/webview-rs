bf412b955d5ef8a78b4fc736850f4f0ade8d225f
2nthony <ant.hu@outlook.com>
2022-01-23 16:23:47 +0800

    chore(script): update release.ts

--- scripts/release.ts
+++ scripts/release.ts
@@ -14,5 +14,5 @@ if (oldVersion === version) {
 
 execSync("git add .", { stdio: "inherit" });
 
-execSync(`git commit -m "chore: release v${version}"`, { stdio: "inherit" });
-execSync(`git tag -a v${version} -m "v${version}"`, { stdio: "inherit" });
+execSync(`git commit -m "${version}"`, { stdio: "inherit" });
+execSync(`git tag -a v${version} -m "${version}"`, { stdio: "inherit" });
