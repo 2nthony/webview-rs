72d63e97b5fa2a0d8c01a430b87680001175da63
2nthony <ant.hu@outlook.com>
2022-01-23 16:29:56 +0800

    chore(script): release update

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,18 +1,10 @@
 import { execSync } from "child_process";
-import { readJSONSync } from "fs-extra";
 
-const { version: oldVersion } = readJSONSync("package.json");
-
-execSync("npx bumpp", { stdio: "inherit" });
-
-const { version } = readJSONSync("package.json");
-
-if (oldVersion === version) {
-  console.log("canceled");
+const newversion = process.argv.slice(2);
+if (!newversion) {
+  console.log("Invalid newversion");
   process.exit();
 }
 
-execSync("git add .", { stdio: "inherit" });
-
-execSync(`git commit -m "${version}"`, { stdio: "inherit" });
-execSync(`git tag -a v${version} -m "${version}"`, { stdio: "inherit" });
+execSync(`npm version ${newversion}`, { stdio: "inherit" });
+execSync("git push --follow-tags", { stdio: "inherit" });
